8eaebd3f8f1304f3d5a62123f9209c518836df4e
To the Moon <110207974+hedera07@users.noreply.github.com>
2022-12-14 15:09:30 +0300

    change drop to silsword.

--- index.js
+++ index.js
@@ -133,17 +133,17 @@ export default e => {
             // type: 'minor',
             type: 'major',
             // start_url: moduleUrls.silk,
-            start_url: 'https://webaverse.github.io/basiheart/index.js',
+            start_url: 'https://webaverse.github.io/silsword/index.js',
             components: [
               {
                 key: 'appName',
                 // value: 'Silk'
-                value: 'basiheart'
+                value: 'silsword'
               },
               {
                 key: 'appUrl',
                 // value: moduleUrls.silk
-                value: 'https://webaverse.github.io/basiheart/index.js'
+                value: 'https://webaverse.github.io/silsword/index.js'
               }
             ],
             position: app.position.clone()
